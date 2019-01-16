e0da30411fb41669ae84cd6e5d2b99464c403aa5
ShellyXueHan <shellshell456@gmail.com>
2019-01-15 21:15:24 -0800

    add routes to update user groups

--- src/router/routes/ssoUsers.js
+++ src/router/routes/ssoUsers.js
@@ -97,4 +97,77 @@ router.get(
   })
 );
 
+router.put(
+  '/user/:userId',
+  asyncMiddleware(async (req, res) => {
+    const { userId } = req.params;
+    const { email, firstName, lastName } = req.query;
+
+    if (!userId) {
+      throw errorWithCode('Please provide the ID of the SSO user you are updating.', 400);
+    }
+
+    if (!email || !firstName || !lastName) {
+      throw errorWithCode('Missing Email, firstName or lastName to update the SSO user', 400);
+    }
+    const userInfo = {
+      id: userId,
+      email,
+      firstName,
+      lastName,
+    };
+
+    logger.info(`Updating user of ${userId}`);
+    const SACredentials = config.get(SSO_REQUEST.SA_CREDENTIAL_NAME);
+    try {
+      const SAToken = await getSAToken(SACredentials);
+      const SSOCredentials = {
+        uri: `${process.env.SSO_HOST_URL}/${SSO_SUB_URI.REALM_ADMIN}/${process.env.SSO_REALM}/`,
+        token: SAToken,
+      };
+      // Update SSO user profile:
+      await updateUser(SSOCredentials, userInfo);
+      // Assingn SSO user to group:
+      await addUserToGroup(SSOCredentials, userId, SSO_GROUPS.PENDING);
+
+      return res.status(200).end();
+    } catch (error) {
+      const message = `Unable to update SSO user with ID ${userId}`;
+      logger.error(`${message}, err = ${error.message}`);
+      throw errorWithCode(`${message}, err = ${error.message}`, 500);
+    }
+  })
+);
+
+router.put(
+  '/user/:userId/confrimed',
+  asyncMiddleware(async (req, res) => {
+    const { userId } = req.params;
+
+    if (!userId) {
+      throw errorWithCode('Please provide the ID of the SSO user you are updating.', 400);
+    }
+
+    logger.info(`Email confirmed user of ${userId}`);
+    const SACredentials = config.get(SSO_REQUEST.SA_CREDENTIAL_NAME);
+    try {
+      const SAToken = await getSAToken(SACredentials);
+      const SSOCredentials = {
+        uri: `${process.env.SSO_HOST_URL}/${SSO_SUB_URI.REALM_ADMIN}/${process.env.SSO_REALM}/`,
+        token: SAToken,
+      };
+      // Remove SSO user from pending group:
+      await removeUserFromGroup(SSOCredentials, userId, SSO_GROUPS.PENDING);
+      // Assingn SSO user to group:
+      await addUserToGroup(SSOCredentials, userId, SSO_GROUPS.REGISTERED);
+
+      return res.status(200).end();
+    } catch (error) {
+      const message = `Unable to update SSO user with ID ${userId}`;
+      logger.error(`${message}, err = ${error.message}`);
+      throw errorWithCode(`${message}, err = ${error.message}`, 500);
+    }
+  })
+);
+
 module.exports = router;
