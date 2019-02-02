882a037aff33b1906f09f20455d88678d54aee50
ShellyXueHan <shellshell456@gmail.com>
2019-02-01 16:11:30 -0800

    update source

--- .pipeline/cli.sh
+++ .pipeline/cli.sh
@@ -4,4 +4,4 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cd $DIR
-curl -sSL 'https://raw.githubusercontent.com/cvarjao-o/pipeline-cli/master/cli.sh' | bash -s "$@"
+curl -sSL 'https://raw.githubusercontent.com/bcdevops/pipeline-cli/master/cli.sh' | bash -s "$@"
