37722b883a69cc778b456ca3bc9176e8c0fe4193
ShellyXueHan <shelly.han@gov.bc.ca>
2020-01-27 12:06:59 -0800

    move repo mountie endpoint url to a secret

--- .pipeline/lib/deploy.js
+++ .pipeline/lib/deploy.js
@@ -14,7 +14,6 @@ module.exports = (settings)=>{
 
   // set the rest of the env vars:
   const extraParams = {
-    RM_HOST_VALUE: 'https://repo-mountie-devhub-prod.pathfinder.gov.bc.ca/bot/github/membership',
     API_URL_VALUE: `${phases[phase].name}${phases[phase].suffix}.pathfinder.gov.bc.ca`,
   };
 
