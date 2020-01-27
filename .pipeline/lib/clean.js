1d07aa871a4152a9d9c5fec4f1c61d8fa233e0ba
ShellyXueHan <shelly.han@gov.bc.ca>
2020-01-27 11:29:49 -0800

    update clean up phase

--- .pipeline/lib/clean.js
+++ .pipeline/lib/clean.js
@@ -5,67 +5,44 @@ module.exports = settings => {
   const phases = settings.phases;
   const options = settings.options;
   const oc = new OpenShiftClientX(Object.assign({ namespace: phases.build.namespace }, options));
-  // const target_phase = options.env;
+
   const build_phase = phases.build.phase;
+  const deploy_phase = options.env;
 
   for (var k in phases) {
     if (phases.hasOwnProperty(k)) {
       const phase = phases[k];
-      // delete dc in tools:
+
+      // Clean up istag in tools:
       if (k == build_phase) {
-        console.log(`phase=${phase}`)
-        oc.raw('get', ['bc'], {selector:`app-name=${phase.name},env-id=${phase.changeId},env-name!=prod,!shared`, namespace:phase.namespace, 'output':'custom-columns=kind:.spec.output.to.kind,name:.spec.output.to.name', 'no-headers':'true'})
+        console.log(`In namespace ${phase.namespace}`);
 
-        oc.raw('get', ['bc'], {selector:`app-name=${phase.name},env-id=${phase.changeId},env-name!=prod,!shared,github-repo=${oc.git.repository},github-owner=${oc.git.owner}`, namespace:phase.namespace, 'output':'custom-columns=kind:.spec.output.to.kind,name:.spec.output.to.name', 'no-headers':'true'})
+        const buildConfigs = oc.get('bc', {
+          selector: `app=${phase.instance},env-id=${phase.changeId},!shared,github-repo=${oc.git.repository},github-owner=${oc.git.owner}`,
+          namespace: phase.namespace,
+        });
 
-        // let buildConfigs = oc.get('bc', {
-        //   selector: `app=${phase.instance},env-id=${phase.changeId},!shared,github-repo=${oc.git.repository},github-owner=${oc.git.owner}`,
-        //   namespace: phase.namespace,
-        // });
+        buildConfigs.forEach(bc => {
+          if (bc.spec.output.to.kind == 'ImageStreamTag') {
+            oc.delete([`ImageStreamTag/${bc.spec.output.to.name}`], {
+              'ignore-not-found': 'true',
+              wait: 'true',
+              namespace: phase.namespace,
+            });
+          }
+        });
+      }
 
-        // buildConfigs.forEach(bc => {
-        //   if (bc.spec.output.to.kind == 'ImageStreamTag') {
-        //     oc.delete([`ImageStreamTag/${bc.spec.output.to.name}`], {
-        //       'ignore-not-found': 'true',
-        //       wait: 'true',
-        //       namespace: phase.namespace,
-        //     });
-        //   }
-        // });
+      // Clean up everything in the temporary deployed env:
+      if (k == deploy_phase) {
+        console.log(`In namespace ${phase.namespace}`);
 
-        // let deploymentConfigs = oc.get('dc', {
-        //   selector: `app=${phase.instance},env-id=${phase.changeId},env-name=${k},!shared,github-repo=${oc.git.repository},github-owner=${oc.git.owner}`,
-        //   namespace: phase.namespace,
-        // });
-        // deploymentConfigs.forEach(dc => {
-        //   dc.spec.triggers.forEach(trigger => {
-        //     if (
-        //       trigger.type == 'ImageChange' &&
-        //       trigger.imageChangeParams.from.kind == 'ImageStreamTag'
-        //     ) {
-        //       oc.delete([`ImageStreamTag/${trigger.imageChangeParams.from.name}`], {
-        //         'ignore-not-found': 'true',
-        //         wait: 'true',
-        //         namespace: phase.namespace,
-        //       });
-        //     }
-        //   });
-        // });
-
-        // oc.raw('delete', ['all'], {
-        //   selector: `app=${phase.instance},env-id=${phase.changeId},!shared,github-repo=${oc.git.repository},github-owner=${oc.git.owner}`,
-        //   wait: 'true',
-        //   namespace: phase.namespace,
-        // });
-        // oc.raw(
-        //   'delete',
-        //   ['pvc,Secret,configmap,endpoints,RoleBinding,role,ServiceAccount,Endpoints'],
-        //   {
-        //     selector: `app=${phase.instance},env-id=${phase.changeId},!shared,github-repo=${oc.git.repository},github-owner=${oc.git.owner}`,
-        //     wait: 'true',
-        //     namespace: phase.namespace,
-        //   },
-        // );
+        oc.raw('delete', ['all,pvc,Secret,configmap,endpoints,RoleBinding,role,ServiceAccount,Endpoints'], {
+          selector: `app=${phase.instance},env-id=${phase.changeId},!shared`,
+          'ignore-not-found': 'true',
+          wait: 'true',
+          namespace: phase.namespace,
+        });
       }
     }
   }
